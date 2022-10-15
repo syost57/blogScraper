cee8a518f88968dd534c3557cbf5b8837398c5b5
syost <syost0101@gmail.com>
2022-10-15 09:40:06 -0400

    Adding discord interactions!

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,23 @@
-import { JSDOM } from 'jsdom';
-import axios from 'axios';
-
-const getBlogDom = async (blogUrl:string) => {
-	return await axios.get(blogUrl);
-}
-
-interface BlogMetaData {
-    author?: string,
-    blogUrl: string,
-    articleHTMLElement: string,
-    previousMostRecentArticle: string,
-    linkHTML: string
-}
-
-const andrewRowe : BlogMetaData = {
-    author: "Andrew Rowe",
-    blogUrl: "https://andrewkrowe.wordpress.com",
-    articleHTMLElement: 'article',
-    previousMostRecentArticle: 'post-2383',
-    linkHTML: 'h1 a'
-}
+import { Client, GatewayIntentBits } from 'discord.js';
+import { fetchBlogUpdates } from './authorBlogs';
+import { initCommands } from './initCommands';
+import configFile from '../config.json';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
-const willWight : BlogMetaData = {
-    author: "Will Wight",
-    blogUrl: "https://www.willwight.com/a-blog-of-dubious-intent",
-    articleHTMLElement: "div.blog-post",
-    previousMostRecentArticle: '    blog-post-403323354847902201',
-    // previousMostRecentArticle: 'blog-post-490735759706768783',
-    linkHTML: 'div h2 a'
-}
-const blogs = [andrewRowe, willWight]
+client.on('ready', () => {
+  console.log(`Logged in as Jeeves!`);
+});
 
-
-const blogScraper = async (blog: BlogMetaData) => {
-    const data = await getBlogDom(blog.blogUrl);
-    const articles = new JSDOM(data.data).window.document;
-    const mostRecentArticle = articles.querySelector(blog.articleHTMLElement);
-
-    if(mostRecentArticle){
-    if(mostRecentArticle.getAttribute('id') !== blog.previousMostRecentArticle) {
-        blog.previousMostRecentArticle = mostRecentArticle.id;
-        return `${blog.author} has a new blog post! Title: ${mostRecentArticle.querySelector(blog.linkHTML)?.textContent} \n${mostRecentArticle.querySelector(blog.linkHTML)}`
-}
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === 'authorblogs') {
+    const authorBlogUpdates = await fetchBlogUpdates();
+    for(const authorBlogUpdate of authorBlogUpdates){
+      authorBlogUpdate === authorBlogUpdates[0] ? await interaction.reply(authorBlogUpdate) : await interaction.followUp(authorBlogUpdate);
     }
 }
-const main = () => {
-    blogs.forEach((blog) => {
-        blogScraper(blog).then((data) => 
-        console.log(data));
-    });
-}
-main();
+});
+
+client.login(configFile.clientToken);
+initCommands();
